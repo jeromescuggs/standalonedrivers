2049c30c7d73f64b34e2c627fadfcbd71fd45f1b
Gol <hypergol@gmail.com>
2014-07-15 15:05:46 +0400

    Added common settings

--- build.sh
+++ build.sh
@@ -1,5 +1,15 @@
-DEVDIR=/opt1/SDK6
+DEVDIR=/opt1/SDK7
+PWD=`pwd`
+ARCH=arm
+CROSS_COMPILE=$DEVDIR/codesourcery/arm-2013.05/bin/arm-none-linux-gnueabi-
+KSRC=$DEVDIR/kernel
+EXTRA_CFLAGS="-DCONFIG_LITTLE_ENDIAN -DCONFIG_PLATFORM_TI_DM365 -I$PWD/include"
+
 export DEVDIR
+export CROSS_COMPILE
+export KSRC
+export EXTRA_CFLAGS
+export ARCH
 
 for path in `ls`
 do
